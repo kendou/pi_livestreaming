45cf551ece7bce49c5f5b650f8f2969304bbce6c
kendou <kenthuang72@gmail.com>
2015-07-27 01:17:39 +0800

    to readable date time string

--- index.js
+++ index.js
@@ -71,9 +71,9 @@ function startStreaming(io) {
 
   fs.watchFile('public/img/image_stream.jpg', function(current, previous) {
     var now = (new Date()).getTime();
-    console.log("New image emitted " + now.toString());
+    console.log("New image emitted " + now.format());
 //    io.sockets.emit('liveStream', 'img/image_stream.jpg?_t=' + (Math.random() * 100000));
-    io.sockets.emit('liveStream', 'img/image_stream.jpg?_t=' + now.toString());
+    io.sockets.emit('liveStream', 'img/image_stream.jpg?_t=' + now.format());
   })
 
 }
