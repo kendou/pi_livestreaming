ee1eb454de254d986d27f6d1a277bc9b13baf93f
kendou <kenthuang72@gmail.com>
2015-07-27 23:17:16 +0800

    change "tl" from 100 ms to 500 ms

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function startStreaming(io) {
     return;
   }
 
-  var args = ["-w", "320", "-h", "240", "-o", "public/img/image_stream.jpg", "-t", "999999999", "-tl", "100"];
+  var args = ["-w", "320", "-h", "240", "-o", "public/img/image_stream.jpg", "-t", "999999999", "-tl", "500"];
   proc = spawn('raspistill', args);
 
   console.log('Watching for changes...');
