081570e296e621601fa07a01a080703e1f916cfb
kentRPI <kenthuang72@gmail.com>
2015-07-29 22:02:22 +0800

    minor change

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ stopStreaming = function() {
 };
 
 startStreaming = function(io) {
-  if(nconf.get('fakeMode') == true){
+  if(nconf.get('fakemode') == true){
     io.sockets.emit('liveStream', imgUrlPathFakeMode);
     return;
   }
